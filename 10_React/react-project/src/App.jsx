b3450737dfbbca0f688c3ee28df3a10f9474e252
DESKTOP-C5J8I0B\user1 <ayinyun@naver.com>
2025-04-22 09:27:19 +0900

    03_react_classcompo update

--- 10_React/react-project/src/App.jsx
+++ 10_React/react-project/src/App.jsx
@@ -1,19 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import JavaScript from './component/JavaScript'
-import Products from './component/Products'
-import Style from './component/Style'
+import LifecycleText from './components/LifecycleText'
+import Comment from './components/Comment';
+import CommentList from './components/CommentList'
 
 function App() {
- 
+  // const [isButton, setIsButton] = useState(true);
+  // const toggleButton = () =>{
+  //   setIsButton(!isButton);
+  // }
 
   return (
     <>
-      
-    {/* <JavaScript/> */}
-    {/* <Style/> */}
-    <Products/>
-    
-    
+      {/* {isButton && <LifecycleText/>}
+      <button onClick={toggleButton}>count없애기</button> */}
+
+      {/* <Comment message={"안녕하세요"}/> */}
+      <CommentList />
     </>
   )
 }
