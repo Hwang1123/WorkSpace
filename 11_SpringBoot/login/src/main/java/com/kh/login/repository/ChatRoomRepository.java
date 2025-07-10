fa4bb780448a15408946c0ae784a52508f91d3dd
DESKTOP-C5J8I0B\user1 <ayinyun@naver.com>
2025-07-10 19:40:30 +0900

    no message

---
+++ 11_SpringBoot/login/src/main/java/com/kh/login/repository/ChatRoomRepository.java
@@ -0,0 +1,9 @@
+package com.kh.login.repository;
+
+import com.kh.login.domain.ChatRoom;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    List<ChatRoom> findByIsGroupChat(String isGroupChat);
+}
